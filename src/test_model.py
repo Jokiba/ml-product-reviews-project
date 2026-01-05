42a9ca7ac65b040b88cc3ca33217857fd33cce89
Jovan_987 <jovanbabic987@gmail.com>
2026-01-05 12:16:05 +0100

    test_model.py added

---
+++ src/test_model.py
@@ -0,0 +1,33 @@
+import joblib
+import pandas as pd
+ 
+# Load the saved model
+model = joblib.load(r"c:\Users\10\Desktop\ITAcademy\Introduction to Machine Learning using Python\Vežbe\lokalni_repozitorijum\ml-product-reviews-project\src\model\sentiment_model.pkl")
+ 
+print("Model loaded successfully!")
+print("Type 'exit' at any point to stop.\n")
+ 
+while True:
+    title = input(" Enter review title: ")
+    if title.lower() == "exit":
+        print("Exiting...")
+        break
+ 
+    text = input(" Enter review text: ")
+    if text.lower() == "exit":
+        print("Exiting...")
+        break
+ 
+    # Compute review length
+    review_length = len(text)
+ 
+    # Create a DataFrame from input
+    user_input = pd.DataFrame([{
+        "review_title": title,
+        "review_text": text,
+        "review_length": review_length
+    }])
+ 
+    # Predict sentiment
+    prediction = model.predict(user_input)[0]
+    print(f" Predicted sentiment: {prediction}\n" + "-" * 40)
